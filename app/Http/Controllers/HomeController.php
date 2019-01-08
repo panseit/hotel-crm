216cd2baebabc3e51ec0e432964356271505078a
karadalex <karadalex@gmail.com>
2019-01-09 01:19:27 +0200

    created basic models and migrations

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\customer;
+use App\Customer;
 
 class HomeController extends Controller
 {
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $customers = customer::all(); 
+        $customers = Customer::all(); 
         return view('home' , ['customers' => $customers]);
         
         
