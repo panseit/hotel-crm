f0554a3e630f16c5c2eca690824b974d73934001
karadalex <karadalex@gmail.com>
2019-01-09 15:18:16 +0200

    Render rooms in a bootstrap table

--- resources/views/rooms.blade.php
+++ resources/views/rooms.blade.php
@@ -8,7 +8,30 @@
                 <div class="card-header">Rooms</div>
 
                 <div class="card-body">
-                    {{ $rooms }}
+                <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Room category</th>
+                                <th scope="col">Start date</th>
+                                <th scope="col">End date</th>
+                                <th scope="col">Adults</th>
+                                <th scope="col">Children</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($rooms as $room)
+                                <tr>
+                                    <th scope="row">{{ $room->room_number }}</th>
+                                    <td>{{ $room->category->room_name }}</td>
+                                    <td>{{ $room->check_in_date }}</td>
+                                    <td>{{ $room->check_out_date }}</td>
+                                    <td>{{ $room->max_adults }}</td>
+                                    <td>{{ $room->max_children }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
