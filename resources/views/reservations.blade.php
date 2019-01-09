34e3739d8dcbc7d8cbc4cbdf8bb91687b7a7a905
karadalex <karadalex@gmail.com>
2019-01-09 14:48:45 +0200

    render reservation in Bootstrap table

--- resources/views/reservations.blade.php
+++ resources/views/reservations.blade.php
@@ -8,7 +8,28 @@
                 <div class="card-header">Reservations</div>
 
                 <div class="card-body">
-                    {{ $reservations }}
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Start date</th>
+                                <th scope="col">End date</th>
+                                <th scope="col">Adults</th>
+                                <th scope="col">Children</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($reservations as $reservation)
+                                <tr>
+                                    <th scope="row">{{ $reservation->reservation_id }}</th>
+                                    <td>{{ $reservation->start_date }}</td>
+                                    <td>{{ $reservation->end_date }}</td>
+                                    <td>{{ $reservation->num_adults }}</td>
+                                    <td>{{ $reservation->num_children }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
