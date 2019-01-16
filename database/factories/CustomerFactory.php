392fefbc2d0ae709aa14fd4a33be6ccc1ac6e869
karadalex <karadalex@gmail.com>
2019-01-16 12:24:08 +0200

    database seeding

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Customer::class, function (Faker $faker) {
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'middle_name' => $faker->name,
+        'country' => $faker->country,
+        'county' => $faker->state,
+        'address_line1' => $faker->address,
+        'address_line2' => $faker->secondaryAddress,
+        'zip_num' => $faker->postcode,
+        'date_of_birth' => $faker->dateTime('1997'),
+        'phone_number' => $faker->e164PhoneNumber,
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        // Ask for db migration refresh, default is no
+        if ($this->command->confirm('Do you wish to refresh migration before seeding, it will clear all old data ?')) 
+        {
+            // Call the php artisan migrate:fresh using Artisan
+            $this->command->call('migrate:fresh');
+
+            $this->command->line("Database cleared.");
+        }
+
+        $this->call([
+            UsersTableSeeder::class
+        ]);
+
+
+
+        $this->command->info("Database seeded.");
     }
 }
